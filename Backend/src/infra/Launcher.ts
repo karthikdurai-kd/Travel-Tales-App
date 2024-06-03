e6e70510b201440d36983557e045926ee49a47fa
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-06-03 19:17:47 -0400

    Fixed Launcher Stack instance

--- Backend/src/infra/Launcher.ts
+++ Backend/src/infra/Launcher.ts
@@ -3,6 +3,7 @@ import { DataStack } from "./stacks/DataStack";
 import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
 import { AuthStack } from "./stacks/AuthStack";
+import { UiDeploymentStack } from "./stacks/UiDeploymentStack";
 
 const app = new App();
 
